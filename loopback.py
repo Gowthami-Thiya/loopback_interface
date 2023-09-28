ba5612beaff7d7c3f3e96513c68a6091e251e997
Gowthami-Thiya <gowthamithiyagrajan@gmail.com>
2023-09-28 17:26:17 +0530

    Created loopback and deleted loop back via python program

--- loopback.py
+++ loopback.py
@@ -1,4 +1,5 @@
 from netmiko import ConnectHandler
+import logging
 login_configs = {
     "device_type":"cisco_xr",
     "ip":"sandbox-iosxr-1.cisco.com",
@@ -11,11 +12,69 @@ def connect_ciscobox():
     try:
         connectivity=ConnectHandler(**login_configs)
         connectivity.enable()
-        command= "ip?"
-        output=connectivity.send_config_set(command)
-        connectivity.disconnect()
+        logging.basicConfig(filename='app.log',filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO)
+        logging.info('Admin logged in')
     except Exception as e:
-        output="Unexpected error occured : {str(e)}"
+        error=f"unable to connect due to this error : {e}"
+        logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.error(error)
+    return connectivity
+
+def show_intefaces():
+    try:
+        connectivity=connect_ciscobox()
+        com="show interfaces"
+        output=connectivity.send_command(com)
+        logging.basicConfig(filename='app.log',filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO)
+        logging.info('listed available interfaces')
+    except Exception as e:
+        error=f"unable to connect due to this error : {e}"
+        output = error
+        logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.error(error)
     return output
-output=connect_ciscobox()
-print(output)
+
+def create_loopback():
+    try:
+        connectivity=connect_ciscobox()
+        command= [
+            "interface Loopback50",
+            "description test loopback config"
+            "ip address 10.0.0.1/28",
+            "commit",
+            "exit"
+        ]
+        response=connectivity.send_config_set(command)
+        logging.basicConfig(filename='app.log',filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO)
+        logging.info('Loopback configured succssfully')
+        print(response)
+    except Exception as e:
+        error=f"unable to connect due to this error : {e}"
+        response=error
+        logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.error(error)
+    return response
+
+def delete_loopback():
+    try:
+        connectivity=connect_ciscobox()
+        delete_interface=["no interface Loopback50",
+            "commit",
+            "exit"]
+        response=connectivity.send_config_set(delete_interface)
+        print(response)
+        logging.basicConfig(filename='app.log',filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO)
+        logging.info('Loopback deleted successfully')
+    except Exception as e:
+        error=f"unable to connect due to this error : {e}"
+        response=error
+        logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.error(error)
+    return response
+
+display=show_intefaces()
+print(display)
+configinterface=create_loopback()
+print(configinterface)
+deleteinterface=delete_loopback()
+print(deleteinterface)
