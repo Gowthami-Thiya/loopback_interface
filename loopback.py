b1556a87fb5cb2973b004b89aef53e5b0e630387
Gowthami-Thiya <gowthamithiyagrajan@gmail.com>
2023-09-28 18:26:01 +0530

    Created endpoints for loopback creation and delete loop back via flask application

--- loopback.py
+++ loopback.py
@@ -1,5 +1,19 @@
+#############################################################################################################################
+# Author : Gowthami Thiyagarajan                                                                                            #
+# Created Date : 27.09.2023                                                                                                 #
+# Description : This Application has been developed to connect with cisco sandbox and cofigure loopback and delete the same #
+# Packages : Flask, netmiko, logging                                                                                        #
+#############################################################################################################################
+
+# Import required packages
+from flask import Flask, request, jsonify
 from netmiko import ConnectHandler
 import logging
+
+# Creating a flask application
+app = Flask(__name__)
+
+# Connection string
 login_configs = {
     "device_type":"cisco_xr",
     "ip":"sandbox-iosxr-1.cisco.com",
@@ -8,6 +22,8 @@ login_configs = {
     "port":22,
     "verbose":True,
 }
+
+# Module to connect with cisco sandbox
 def connect_ciscobox():
     try:
         connectivity=ConnectHandler(**login_configs)
@@ -20,11 +36,15 @@ def connect_ciscobox():
         logging.error(error)
     return connectivity
 
+# Module to list available interfaces
 def show_intefaces():
     try:
         connectivity=connect_ciscobox()
+
+        # command to list interfaces
         com="show interfaces"
         output=connectivity.send_command(com)
+        connectivity.disconnect()
         logging.basicConfig(filename='app.log',filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO)
         logging.info('listed available interfaces')
     except Exception as e:
@@ -34,47 +54,87 @@ def show_intefaces():
         logging.error(error)
     return output
 
-def create_loopback():
+# Module to create Loopback in cisco device
+def create_loopback(interface_number,desc,ipaddress):
     try:
         connectivity=connect_ciscobox()
+
+        # commands to create loopback
         command= [
-            "interface Loopback50",
-            "description test loopback config"
-            "ip address 10.0.0.1/28",
+            f"interface {interface_number}",
+            f"description {desc}"
+            f"ip address {ipaddress}",
             "commit",
             "exit"
         ]
         response=connectivity.send_config_set(command)
+        connectivity.disconnect()
         logging.basicConfig(filename='app.log',filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO)
         logging.info('Loopback configured succssfully')
-        print(response)
+        return {'message': 'Configuration done successfully:', 'response': response}, 200
     except Exception as e:
         error=f"unable to connect due to this error : {e}"
         response=error
         logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
         logging.error(error)
-    return response
-
-def delete_loopback():
+        return {'message': str(e)}, 500
+    
+# Module to delete the created loopback
+def delete_loopback(interface_number):
     try:
         connectivity=connect_ciscobox()
-        delete_interface=["no interface Loopback50",
+        
+        # Commands to delete the loopback
+        delete_interface=[f"no interface {interface_number}",
             "commit",
             "exit"]
         response=connectivity.send_config_set(delete_interface)
         print(response)
         logging.basicConfig(filename='app.log',filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO)
         logging.info('Loopback deleted successfully')
+        return {'message': 'Loopback Configuration deleted successfully:', 'response': response}, 200
     except Exception as e:
         error=f"unable to connect due to this error : {e}"
         response=error
         logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
         logging.error(error)
-    return response
+        return {'message': str(e)}, 500
 
-display=show_intefaces()
-print(display)
-configinterface=create_loopback()
-print(configinterface)
-deleteinterface=delete_loopback()
-print(deleteinterface)
+###############
+#   Enpoints  #
+###############
+
+# Route to configure loopback with the passed payload
+@app.route('/loopback_configuration', methods=['POST'])
+def loopback_configuration():
+    payload = request.json
+    #device_params=connectionstring(data)
+    ipaddress = payload.get('loopback_ip')
+    interface_number = payload.get('interface_number')
+    desc=payload.get('description')
+    
+    response = create_loopback(interface_number,desc,ipaddress)
+
+    return jsonify({'response': response})
+
+# Route to List all the interfaces
+@app.route('/list_interfaces', methods=['GET'])
+def list_interfaces():
+    
+    result = show_intefaces()
+
+    return jsonify({'result': result})
+
+# Route to delete the loopback based on passed payload
+@app.route('/remove_loopback', methods=['POST'])
+def remove_loopback():
+    payload = request.json
+    #device_params=connectionstring(data)
+    interface_number = payload.get('interface_number')
+    
+    response = delete_loopback(interface_number)
+
+    return jsonify({'response': response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
